5c64fbf4c282fe34e4d9e35a48b364799f443c57
PrasunPK <prasun_1993@rediffmail.com>
2017-02-08 21:40:47 +0530

    Spike | rendering static value to structurize the matrix render process

--- pairing_matrix/matrix.js
+++ pairing_matrix/matrix.js
@@ -1,9 +1,25 @@
-var dimension = 10;
+var names = ['A','B','C','D', 'E'];
+var dimension = names.length;
 $(document).ready(function() {
-	$("#matrix").css('width', (dimension*52));
+	for(var y = 0; y <= dimension; y++) {
+		var unit;
+		if (y == 0) {
+        	unit = $("<div class='h-identifier'></div>");
+        }else { 
+        	unit = $("<div class='h-identifier'> "+ names[y-1] +"</div>");
+        }
+        unit.appendTo('#horizontal-identifiers');
+    }
+
+	$("#matrix").css('width', ((dimension+1)*52));
     for(var x = 0; x < dimension; x++) {
-        for(var y = 0; y < dimension; y++) {
-            var unit = $("<div class='unit'> "+ x + " " + y +"</div>");
+        for(var y = 0; y <= dimension; y++) {
+        	var unit;
+        	if (y == 0) {
+            	unit = $("<div class='unit'> "+ names[x] +"</div>");
+        	}else{
+        		unit = $("<div class='unit'> "+ x + " " + y +"</div>");
+        	}
             unit.appendTo('#matrix');
         }
     }
